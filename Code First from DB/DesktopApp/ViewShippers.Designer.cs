e20d6a8200b62765750d92447793553e98dfeed4
hoangker <tin@tinmanmedia.ca>
2014-06-11 15:50:10 -0600

    Some View Shipper Functionatliy

--- Code First from DB/DesktopApp/ViewShippers.Designer.cs
+++ Code First from DB/DesktopApp/ViewShippers.Designer.cs
@@ -28,19 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboShippers = new System.Windows.Forms.ComboBox();
+            this.LookupBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblShipperID = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shippers:";
+            // 
+            // cboShippers
+            // 
+            this.cboShippers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboShippers.FormattingEnabled = true;
+            this.cboShippers.Location = new System.Drawing.Point(118, 10);
+            this.cboShippers.Name = "cboShippers";
+            this.cboShippers.Size = new System.Drawing.Size(121, 21);
+            this.cboShippers.TabIndex = 1;
+            this.cboShippers.SelectedIndexChanged += new System.EventHandler(this.cboShippers_SelectedIndexChanged);
+            // 
+            // LookupBtn
+            // 
+            this.LookupBtn.Location = new System.Drawing.Point(245, 8);
+            this.LookupBtn.Name = "LookupBtn";
+            this.LookupBtn.Size = new System.Drawing.Size(102, 23);
+            this.LookupBtn.TabIndex = 2;
+            this.LookupBtn.Text = "Lookup Shipper";
+            this.LookupBtn.UseVisualStyleBackColor = true;
+            this.LookupBtn.Click += new System.EventHandler(this.LookupBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Shipper ID:";
+            // 
+            // lblShipperID
+            // 
+            this.lblShipperID.AutoSize = true;
+            this.lblShipperID.Location = new System.Drawing.Point(115, 65);
+            this.lblShipperID.Name = "lblShipperID";
+            this.lblShipperID.Size = new System.Drawing.Size(64, 13);
+            this.lblShipperID.TabIndex = 4;
+            this.lblShipperID.Text = "lblShipperID";
+            this.lblShipperID.Click += new System.EventHandler(this.lblShipperID_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Company Name:";
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(118, 99);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(100, 20);
+            this.txtCompanyName.TabIndex = 6;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(118, 141);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtPhone.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Phone:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(16, 199);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(57, 23);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(84, 199);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(57, 23);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(147, 199);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(57, 23);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(210, 199);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(57, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
             // 
             // ViewShippers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(354, 261);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblShipperID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LookupBtn);
+            this.Controls.Add(this.cboShippers);
+            this.Controls.Add(this.label1);
             this.Name = "ViewShippers";
             this.Text = "View/Edit Shippers";
+            this.Load += new System.EventHandler(this.ViewShippers_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboShippers;
+        private System.Windows.Forms.Button LookupBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblShipperID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
     }
 }
--- Code First from DB/DesktopApp/ViewShippers.cs
+++ Code First from DB/DesktopApp/ViewShippers.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NorthwindSystem.BLL;
+using NorthwindSystem.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +18,115 @@ namespace DesktopApp
         {
             InitializeComponent();
         }
+
+        private void lblShipperID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void ViewShippers_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                PopulateShippersComboBox();
+            }
+            catch (Exception ex)
+            { 
+                //TODO: Log the exception
+                MessageBox.Show("Error: " + ex.Message);
+            
+            }
+        }
+
+        private void PopulateShippersComboBox()
+        {
+            NorthwindManager manager = new NorthwindManager();
+            var data = manager.ListShippers();
+            data.Insert(0, new Shipper()
+            {
+                ShipperID = -1,
+                CompanyName = "[Select a shipper]"
+            });
+            cboShippers.DataSource = data;
+            cboShippers.DisplayMember = "CompanyName"; // CompanyName is a property of the Shipper class
+            cboShippers.ValueMember = "ShipperID"; // ShipperID is the property that represents the Primary Key (uniquely distinguishes each shipper in the database)
+            //cboShippers.Items.Insert(0, "[Select a shipper]");
+            cboShippers.SelectedIndex = 0;
+        }
+
+        private void cboShippers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cboShippers.SelectedIndex != 0)
+                {
+                    lblShipperID.Text = cboShippers.SelectedValue.ToString();
+                    //txtCompanyName.Text = cboShippers.SelectedItem.ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //TODO: Log the exception
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void LookupBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cboShippers.SelectedIndex <= 0)
+                {
+                    MessageBox.Show("Please select a shipper before clicking [Lookup Shipper]");
+                }
+                else 
+                {
+                    int shipperId = Convert.ToInt32(cboShippers.SelectedValue);
+                    NorthwindManager mgr = new NorthwindManager();
+                    var shipper = mgr.GetShipper(shipperId);
+                    if (shipper != null)
+                    {
+                        lblShipperID.Text = shipper.ShipperID.ToString();
+                        txtCompanyName.Text = shipper.CompanyName;
+                        txtPhone.Text = shipper.Phone;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                //TODO: Log the exception
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NorthwindManager mgr = new NorthwindManager();
+
+               int shipperId =  mgr.AddShipper(new Shipper()
+                                {
+                                    CompanyName=txtCompanyName.Text,
+                                    Phone=txtPhone.Text
+                                });
+
+                //update the combobox
+               PopulateShippersComboBox();
+                //has right shipper selected
+               cboShippers.SelectedValue = shipperId;
+                //display id of added shipper
+               lblShipperID.Text = shipperId.ToString();
+            }
+            catch (Exception ex)
+            {
+                //TODO: Log the exception
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+        
     }
 }
