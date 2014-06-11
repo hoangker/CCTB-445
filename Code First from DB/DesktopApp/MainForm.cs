66ed98c528770092ce2cec713a1e58de6fbd95d4
hoangker <tin@tinmanmedia.ca>
2014-06-11 14:35:24 -0600

    Created View Shipper form

---
+++ Code First from DB/DesktopApp/MainForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO: Close the program/form
+        }
+
+        private void regionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO: Open a form as a dialog box
+            ViewRegions frm = new ViewRegions();
+
+            frm.ShowDialog(); // Execution of this method will PAUSE here until the dialog box (ViewRegions) is closed
+            // resume after the dialog box is closed
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //Set the applications startup date/time in the status bar
+            StartTimeStatus.Text = "App Started on " + DateTime.Now.ToString();
+        }
+
+        private void productsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void shippersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ViewShippers shippersfrm = new ViewShippers();
+            shippersfrm.MdiParent = this;
+            shippersfrm.WindowState = FormWindowState.Maximized;
+            shippersfrm.Show(); // we do NOT pause here as we show the form..
+            MessageBox.Show("Here's the ViewShopper's Form");
+        }
+
+        private void customerOrdersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void productSalesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void errorLogsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutThisAppToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //TODO: 1) Open the AboutApp form as a dialog window.
+            AboutApp aboutApp = new AboutApp();
+            aboutApp.ShowDialog();
+        }
+    }
+}
---
+++ Code First from DB/DesktopApp/ViewShippers.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace DesktopApp
+{
+    partial class ViewShippers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ViewShippers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 261);
+            this.Name = "ViewShippers";
+            this.Text = "View/Edit Shippers";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
